851996f98b8caf48a9c5130e8d4ec620033f4480
unknown <you@example.com>
2021-05-22 21:45:13 -0500

    some note

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -1,7 +1,23 @@
-select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+-- 1) List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+select e.emp_no, 
+	e.last_name, 
+	e.first_name,
+	e.sex, 
+	s.salary
 from employees as e
 left join salaries as s
 on e.emp_no = s.emp_no;
+
+-- 2) List first name, last name, and hire date for employees who were hired in 1986
+
+select first_name,
+	last_name, 
+	hire_date
+from employees
+where hire_date between '1986/1/1' and '1986/12/31';
+
+-- 3) 
 
 
 
