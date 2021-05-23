e641c0baa08b5fedd4b4b663f8521e4315890dad
unknown <you@example.com>
2021-05-22 22:34:59 -0500

    some note

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -17,7 +17,34 @@ select first_name,
 from employees
 where hire_date between '1986/1/1' and '1986/12/31';
 
--- 3) 
+-- 3) List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+select d.dept_no,
+	d.dept_name, 
+	dm.emp_no, 
+	e.last_name, 
+	e.first_name
+from departments as d
+left join dept_manager as dm
+on dm.dept_no = d.dept_no
+left join employees as e
+on dm.emp_no = e.emp_no;
+
+
+--4) List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+select e.emp_no, 
+	e.last_name, 
+	e.first_name, 
+	d.dept_name
+from dept_emp as de
+left join departments as d
+on de.dept_no = d.dept_no
+left join employees as e
+on de.emp_no = e.emp_no	;
+
+	
+	
 
 
 
